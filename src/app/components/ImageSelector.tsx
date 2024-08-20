f44495fdb425d90e1d4db855d12c58ad7881e9fb
송이현 <songehyun00@gmail.com>
2024-08-20 10:25:16 +0900

    이미지 아래 숫자 추가

--- src/app/components/ImageSelector.tsx
+++ src/app/components/ImageSelector.tsx
@@ -24,33 +24,35 @@ export default function ImageSelector() {
 
   return (
     <div className="flex justify-center items-center space-x-4">
-      <div className="relative">
+      <div className="relative flex flex-col items-center">
         <Image
-          src="/src/img/Dimg01.jpg"
+          src="/dummy1.png"
           alt="Dummy 1"
           width={300}
           height={300}
           className={`border-4 ${
-            selectedImage === 1 ? 'border-green-500' : 'border-transparent'
+            selectedImage === 1 ? 'border-blue-500' : 'border-transparent'
           }`}
         />
         {selectedImage === 1 && (
-          <div className="absolute inset-0 border-3 border-green-500 pointer-events-none"></div>
+          <div className="absolute inset-0 border-4 border-blue-500 pointer-events-none"></div>
         )}
+        <p className="mt-2 text-lg font-semibold">10</p> {/* 숫자 10 추가 */}
       </div>
-      <div className="relative">
+      <div className="relative flex flex-col items-center">
         <Image
-          src="/src/img/Dimg02.jpg"
+          src="/dummy2.png"
           alt="Dummy 2"
           width={300}
           height={300}
           className={`border-4 ${
-            selectedImage === 2 ? 'border-green-500' : 'border-transparent'
+            selectedImage === 2 ? 'border-blue-500' : 'border-transparent'
           }`}
         />
         {selectedImage === 2 && (
-          <div className="absolute inset-0 border-3 border-green-500 pointer-events-none"></div>
+          <div className="absolute inset-0 border-4 border-blue-500 pointer-events-none"></div>
         )}
+        <p className="mt-2 text-lg font-semibold">100</p> {/* 숫자 100 추가 */}
       </div>
     </div>
   );
